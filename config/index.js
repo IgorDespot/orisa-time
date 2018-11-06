cc01504891cc8c10a1c67b1a45a775be93130d07
IgorDespot <igordesspot@gmail.com>
2018-11-06 01:29:01 -0800

    Add configuration infrastructure

---
+++ config/index.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+module.exports = {
+    googleApiKey: process.env.GOOGLE_GEO_API_KEY,
+};
--- server/service.js
+++ server/service.js
@@ -1,18 +1,14 @@
 'use strict';
 
+const config = require('../config');
+
 const express = require('express');
 const service = express();
 const request = require('superagent');
 const moment = require('moment');
 
-const API = 'AIzaSyC2lenB028Fiz-60ypyDaJnFjur2u-JiYE';
-
-// https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyC2lenB028Fiz-60ypyDaJnFjur2u-JiYE
-// https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000&key=AIzaSyC2lenB028Fiz-60ypyDaJnFjur2u-JiYE
-
-
 service.get('/service/:location', (req, res, next) => {
-    request.get('https://maps.googleapis.com/maps/api/geocode/json?address='+ req.params.location + '&key=AIzaSyC2lenB028Fiz-60ypyDaJnFjur2u-JiYE', (err, response) => {
+    request.get('https://maps.googleapis.com/maps/api/geocode/json?address='+ req.params.location + '&key=' + config.googleApiKey, (err, response) => {
         if (err) {
             console.log(err);
             return res.status(500);
@@ -21,7 +17,7 @@ service.get('/service/:location', (req, res, next) => {
         let location = response.body.results[0].geometry.location;
         let timestamp = +moment().format('X');
 
-        request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng + '&timestamp=' + timestamp + '&key=AIzaSyC2lenB028Fiz-60ypyDaJnFjur2u-JiYE', (err, response) => {
+        request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng + '&timestamp=' + timestamp + '&key=' + config.googleApiKey, (err, response) => {
             if (err) {
                 console.log(err);
                 return res.status(500);
